d2646b31280eb0c3815315c7e6e3a35f9ec6d661
moocow <mortens@gmail.com>
2019-03-19 22:49:48 +0100

    initial app implementation

--- main.go
+++ main.go
@@ -1,10 +1,84 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
 	"github.com/kimpettersen/svc-voicepayments/pkg/paymentsclient"
 )
 
 func main() {
+	get := flag.String("get", "", "get specific payment by id")
+	confirm := flag.String("confirm", "", "id of payment to confirm")
+	all := flag.Bool("all", false, "get all payments")
+
+	to := flag.String("to", "", "user to send payment to")
+	from := flag.String("from", "", "user to send payment from")
+	amount := flag.Int64("amount", 0, "amount to send")
+
+	flag.Parse()
+
 	paymentsClient := paymentsclient.New()
-	paymentsClient.MakePayment()
+
+	if *get != "" {
+		payment, err := paymentsClient.GetByID(*get)
+		if err != nil {
+			log.Println(err)
+			os.Exit(1)
+		}
+
+		fmt.Printf("Payment from %s to %s of amount %d\n",
+			payment.From,
+			payment.To,
+			payment.Amount)
+
+		return
+	}
+
+	if *confirm != "" {
+		payment, err := paymentsClient.ConfirmPayment(*confirm)
+		if err != nil {
+			log.Println(err)
+			os.Exit(1)
+		}
+		fmt.Printf("Payment of id %s confirmed", payment.Id)
+
+		return
+	}
+
+	if *all == true {
+		payments, err := paymentsClient.GetAll()
+		if err != nil {
+			log.Println(err)
+			os.Exit(1)
+		}
+
+		for i := 0; i < len(payments.Payments); i++ {
+			fmt.Printf("%d. Payment from %s to %s of amount %d (%s)\n",
+				i,
+				payments.Payments[i].From,
+				payments.Payments[i].To,
+				payments.Payments[i].Amount,
+				payments.Payments[i].Id)
+		}
+
+		return
+	}
+
+	if *to != "" && *from != "" && *amount != 0 {
+		payment, err := paymentsClient.MakePayment(*amount, *from, *to)
+
+		if err != nil {
+			log.Println(err)
+			os.Exit(1)
+		}
+
+		fmt.Printf("Please confirm payment: %s\n", payment.Id)
+
+		return
+	}
+
+	flag.Usage()
 }
--- pkg/paymentsclient/paymentsclient.go
+++ pkg/paymentsclient/paymentsclient.go
@@ -1,6 +1,8 @@
 package paymentsclient
 
 import (
+	"context"
+	"errors"
 	"log"
 
 	pb "github.com/kimpettersen/svc-payments/proto"
@@ -27,6 +29,64 @@ func New() *PaymentsClient {
 }
 
 // MakePayment calls the external gRPC endpoint to make a payment
-func (a PaymentsClient) MakePayment() {
-	// TODO: Make a remote call to the payment server and make a payment
+func (a PaymentsClient) MakePayment(amount int64, from string, to string) (pb.Payment, error) {
+	// make the payment request
+	paymentRequest := pb.PaymentRequest{
+		Amount: amount,
+		From:   from,
+		To:     to,
+	}
+
+	payment, err := a.externalClient.Pay(context.Background(), &paymentRequest)
+
+	if err != nil {
+		log.Println(err)
+
+		return pb.Payment{}, errors.New("Failed to send payment")
+	}
+
+	return *payment, nil
+}
+
+// ConfirmPayment confirms a payment
+func (a PaymentsClient) ConfirmPayment(id string) (pb.Payment, error) {
+	paymentByID := pb.PaymentByIdRequest{
+		Id: id,
+	}
+
+	confirm, err := a.externalClient.Confirm(context.Background(), &paymentByID)
+
+	if err != nil {
+		return pb.Payment{}, errors.New("Failed to confirm payment")
+	}
+
+	return *confirm, nil
+}
+
+// GetByID gets a specific payment by id
+func (a PaymentsClient) GetByID(id string) (pb.Payment, error) {
+	paymentByID := pb.PaymentByIdRequest{
+		Id: id,
+	}
+	payment, err := a.externalClient.GetById(context.Background(), &paymentByID)
+
+	if err != nil {
+		log.Panicln(err)
+		return pb.Payment{}, errors.New("Failed to get payment by id")
+	}
+	return *payment, nil
+}
+
+// GetAll gets all payments made through service
+func (a PaymentsClient) GetAll() (pb.PaymentList, error) {
+	all := pb.AllPaymentsRequest{}
+
+	payments, err := a.externalClient.GetAll(context.Background(), &all)
+
+	if err != nil {
+		log.Println(err)
+		return pb.PaymentList{}, errors.New("Failed to get all payments")
+	}
+
+	return *payments, nil
 }
